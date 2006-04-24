a6c8e6e56437537ad8cbc358e548d608afa695c0
toedter_k <toedter_k@localhost>
2006-04-24 14:11:27 +0000

    looks 2.0.1

--- JCalendar/bin/runJCalendarDemo.bat
+++ JCalendar/bin/runJCalendarDemo.bat
@@ -1,1 +1,1 @@
-javaw -classpath %classpath%;../lib/jcalendar-@VERSION@.jar;../lib/looks-1.3.2.jar com.toedter.calendar.demo.JCalendarDemo
+javaw -classpath %classpath%;../lib/jcalendar-@VERSION@.jar;../lib/looks-2.0.1.jar com.toedter.calendar.demo.JCalendarDemo
