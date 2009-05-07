0388e24fef6b68d212d9205173967937cf97436c
toedter_k <toedter_k@localhost>
2009-05-07 04:26:12 +0000

    removed @override

--- JCalendar/src/com/toedter/calendar/JDateChooser.java
+++ JCalendar/src/com/toedter/calendar/JDateChooser.java
@@ -547,7 +547,6 @@ public class JDateChooser extends JPanel implements ActionListener,
 		changeListener = null;
 	}
 
-	@Override
 	public boolean requestFocusInWindow() {
 		if (dateEditor instanceof JComponent) {
 			return ((JComponent) dateEditor).requestFocusInWindow();
