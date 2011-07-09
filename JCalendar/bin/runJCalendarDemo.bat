3620eaf45c366bdcd447a67182b694e66a2aca67
toedter_k <toedter_k@localhost>
2011-07-09 10:13:33 +0000

    classpath bug fixed

--- JCalendar/bin/runJCalendarDemo.bat
+++ JCalendar/bin/runJCalendarDemo.bat
@@ -1,1 +1,1 @@
-javaw -classpath %classpath%;../lib/jcalendar-@VERSION@.jar;../lib/jgoodies-looks-2.4.1.jar;../lib/jgoodies-common-1.2.0.jar com.toedter.calendar.demo.JCalendarDemo
+javaw -classpath ../lib/jcalendar-@VERSION@.jar;../lib/jgoodies-looks-2.4.1.jar;../lib/jgoodies-common-1.2.0.jar com.toedter.calendar.demo.JCalendarDemo
